58a5430329af36e12a250f2a9d8853d8c7bd2ad1
DovydasVad <vadisius.dovydas@gmail.com>
2024-05-02 14:38:22 +0200

    Structure: move algorithms and models into their own folders

---
+++ algorithms/algorithm.py
@@ -0,0 +1,9 @@
+class Algorithm:
+    def __init__(self):
+        pass
+
+    def get_probe_input(self):
+        pass
+
+    def answer(self, probe_result):
+        pass
---
+++ algorithms/one_path.py
@@ -0,0 +1,16 @@
+import math
+from algorithm import Algorithm
+
+"""
+During a phase of length 2R, two balls around start and end vertices are grown.
+If these two balls intersect, there is a path between start and end vertices.
+"""
+class AlgorithmOnePath(Algorithm):
+    def __init__(self, c, n, start_v, end_v):
+        #self.R = math.ceil(math.sqrt(c * n))
+
+    def get_probe_input(self):
+        pass
+
+    def answer(self, probe_result):
+        pass
