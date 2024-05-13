1e343ee534590b746196f06ba2ee7b1c477e6d84
DovydasVad <vadisius.dovydas@gmail.com>
2024-05-13 02:09:21 +0200

    Add: one_path algorithm implementation

--- algorithms/one_path.py
+++ algorithms/one_path.py
@@ -10,12 +10,62 @@ class AlgorithmOnePath(Algorithm):
         self.R = math.ceil(math.sqrt((c * n)/math.log(n)))
         self.start_vertex = 0
         self.end_vertex = n - 1
+        self.last_path = []
+        self.phase_position = 0
+        self.ball_S = []
+        self.ball_T = []
+        self.parent_S = [-1 for _ in range(n)]
+        self.parent_T = [-1 for _ in range(n)]
 
     def get_probe_input(self):
-        return NotImplementedError
+        if self.phase_position == 0:
+            self.ball_S = [self.start_vertex]
+            self.curr_ball = self.ball_S
+        elif self.phase_position == self.R:
+            self.ball_T = [self.end_vertex]
+            self.curr_ball = self.ball_T
+
+        # it is intended to repeatedly take the last vertex if the ball is not growing anymore, as this is a simple implementation as described in the paper
+        probe_vertex = self.curr_ball[min(self.phase_position % self.R, len(self.curr_ball) - 1)]
+        self.last_probe = probe_vertex
+        return probe_vertex
 
     def set_probe_result(self, probe_result):
-        return NotImplementedError
+        for v in probe_result:
+            if v not in self.curr_ball:
+                self.curr_ball.append(v)
+                if self.phase_position < self.R:
+                    self.parent_S[v] = self.last_probe
+                else:
+                    self.parent_T[v] = self.last_probe
+
+
+        self.phase_position = self.phase_position + 1
+        if self.phase_position == 2 * self.R:
+            ball_intersection = list(set(self.ball_S) & set(self.ball_T))
+            if ball_intersection == []:
+                self.last_path = []
+            else:
+                middle_vertex = ball_intersection[0]
+                self.last_path = []
+                curr_vertex = middle_vertex
+                while curr_vertex != self.start_vertex:
+                    next_vertex = self.parent_S[curr_vertex]
+                    self.last_path.append(next_vertex)
+                    curr_vertex = next_vertex
+                self.last_path.reverse()
+                self.last_path.append(middle_vertex)
+                
+                curr_vertex = middle_vertex
+                while curr_vertex != self.end_vertex:
+                    next_vertex = self.parent_T[curr_vertex]
+                    self.last_path.append(next_vertex)
+                    curr_vertex = next_vertex
+
+            self.phase_position = 0
+        #print("Balls")
+        #print(self.ball_S)
+        #print(self.ball_T)
 
     def answer(self):
-        return NotImplementedError
+        return self.last_path
