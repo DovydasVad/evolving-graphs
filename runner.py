73f464d6327ce422c7d88f174e9481dd684e2f9f
DovydasVad <vadisius.dovydas@gmail.com>
2024-06-28 17:48:19 +0200

    Add: import and update edges through Runner

--- runner.py
+++ runner.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from algorithms.algorithm import Algorithm
 
 def visualize_result(algorithm: Algorithm, answer_correct: bool):
@@ -13,7 +14,7 @@ def visualize_result(algorithm: Algorithm, answer_correct: bool):
 
 
 class Runner:
-    def __init__(self, probe_rate, change_rate, algorithm, graph):
+    def __init__(self, probe_rate, change_rate, algorithm, graph, use_dataset = False, dataset = None):
         self.correct_answers = 0
         self.correct_answers_after_1st_phase = 0
         self.total_iterations = 0
@@ -21,10 +22,17 @@ class Runner:
         self.change_rate = change_rate
         self.algorithm = algorithm
         self.graph = graph
+        self.use_dataset = use_dataset
+        self.dataset = dataset
+        if use_dataset:
+            algorithm.set_start_vertex(dataset["start_vertex"])
+            algorithm.set_end_vertex(dataset["end_vertex"])
+            graph.set_start_vertex(dataset["start_vertex"])
+            graph.set_end_vertex(dataset["end_vertex"])
     
     def run(self, iterations, visualization_step):
         self.total_iterations += iterations
-        for iteration in range(iterations):
+        for iteration in tqdm(range(iterations), disable = (not self.use_dataset)):
             # perform probes
             for j in range(self.probe_rate):
                 v = self.algorithm.get_probe_input()
@@ -41,8 +49,14 @@ class Runner:
                 visualize_result(self.algorithm, answer_correct)
             
             # perform changes in the model
-            for j in range(self.change_rate):
-                self.graph.change()
+            if self.use_dataset:
+                if iteration == 0:
+                    self.graph.import_edges(self.dataset["edges"][iteration])
+                else:
+                    self.graph.update_edges(self.dataset["new_edges"][iteration], self.dataset["removed_edges"][iteration])
+            else:
+                for j in range(self.change_rate):
+                    self.graph.change()
     
     def get_total_iterations(self):
         return self.total_iterations
