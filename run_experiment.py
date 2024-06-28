3968f41a3aca851bd45049c7ad639e4f9cd77a36
DovydasVad <vadisius.dovydas@gmail.com>
2024-06-28 17:46:34 +0200

    Add: option to run a dataset

--- run_experiment.py
+++ run_experiment.py
@@ -6,12 +6,17 @@ Multiple iterations are performed, in a single iteration:
     3) model validates the answer
     4) model makes change_rate changes
 
-Calling example:
-python3 run_experiment.py --alg=one --n=1000 --m=15000 --c0=0.5 --iterations=10000 --change=1 --probe=1 --rand_seed=0
+Calling example for ER graphs:
+python3 run_experiment.py --alg=one --n=1000 --m=15000 --c0=0.5 --iterations=10000 --change=1 --probe=1 --rand_seed=0 --model=basic
+
+Calling example for dataset:
+python3 run_experiment.py --alg=one --c0=0.5 --change=1 --probe=5 --dataset=wikipedia
 """
 
 import argparse
 import math
+import os
+import pickle
 
 from models.unweighted_model import UnweightedGraph
 from models.unweighted_model_e import UnweightedGraphE
@@ -25,13 +30,14 @@ from runner import Runner
 def parse_args():
     parser = argparse.ArgumentParser(description = 'Run the experiments: interaction between changing model and the algorithm.')
     parser.add_argument('--alg', dest = 'alg', type = str, required = True, help = 'Algorithm used for testing, one of: one (one_path), two (two_path)')
-    parser.add_argument('--n', dest = 'n', type = int, required = True, help = 'Number of vertices in the graph')
-    parser.add_argument('--m', dest = 'm', type = int, required = True, help = 'Number of edges in the graph')
+    parser.add_argument('--n', dest = 'n', type = int, default = 1000, help = 'Number of vertices in the graph')
+    parser.add_argument('--m', dest = 'm', type = int, default = 5000, help = 'Number of edges in the graph')
     parser.add_argument('--c0', dest = 'c0', type = float, required = True, help = 'Constant c0 used to define constant R (used for phase_length and ball growth step duration)')
     parser.add_argument('--change', dest = 'change_rate', type = int, default = 1, help = 'Number of changes that graph makes at once (default = 1)')
     parser.add_argument('--probe', dest = 'probe_rate', type = int, default = 1, help = 'Number of probes that the algorithm is allowed to make at once (default = 1)')
-    parser.add_argument('--iterations', dest = 'iterations', type = int, required = True, help = 'Number of iterations performed')
+    parser.add_argument('--iterations', dest = 'iterations', type = int, default = 10000, help = 'Number of iterations performed')
     parser.add_argument('--model', dest = 'model', type = str, default = "", help = "Configuration of the model ('e' and 'v' include edge and vertex removals, respectively)")
+    parser.add_argument('--dataset', dest = 'dataset', type = str, default = "", help = "Dataset for experiment ('' for a random graph, or 'contact', 'wikipedia')")
     parser.add_argument('--rand_seed', dest = 'rand_seed', type = int, default = 0, help = 'Random seed used for reproducibility (default = 0)')
     parser.add_argument('--visualization', dest = 'visualization_step', type = int, default = -1, help = 'every <visualization_step> iterations, prints a character indicating the validity of answer provided by the algorithm (default = -1 (not active))')
     return parser.parse_args()
@@ -42,27 +48,57 @@ def print_alg_info(algorithm: Algorithm):
     print("--- {} algorithm ---".format(algorithm.name))
     print('R = {}, phase length = {}'.format(algorithm.R, algorithm.phase_length))
 
+n = args.n
+m = args.m
+iterations = args.iterations
+initialize_graph = True
+use_dataset = False
+dataset = None
+if "contact" in args.dataset:
+    use_dataset = True
+    with open(os.path.join("datasets", "contact", "dataset.pkl"), "rb") as fin:
+        dataset = pickle.load(fin)
+        n = 789
+        m = 0
+    iterations = len(dataset["edges"])
+    initialize_graph = False
+elif "wikipedia" in args.dataset:
+    use_dataset = True
+    print("Loading dataset...")
+    with open(os.path.join("datasets", "wikipedia", "dataset.pkl"), "rb") as fin:
+        dataset = pickle.load(fin)
+        n = 100312
+        m = 0
+    print("Dataset loaded.")
+    iterations = len(dataset["edges"])
+    initialize_graph = False
+elif args.dataset != "":
+    print("Wrong dataset name! Use 'contact' or 'wikipedia'.")
+
 if args.alg.startswith("one"):
-    algorithm = AlgorithmOnePath(args.c0, args.n)
+    algorithm = AlgorithmOnePath(args.c0, n)
     print_alg_info(algorithm)
-if args.alg.startswith("two"):
-    algorithm = AlgorithmTwoPath(args.c0, args.n)
+elif args.alg.startswith("two"):
+    algorithm = AlgorithmTwoPath(args.c0, n)
     print_alg_info(algorithm)
+else:
+    print("Wrong algorithm name! Use 'one' for one-path algorithm, or 'two' for two-path algorithm.")
 
 if "e" in args.model and "v" in args.model:
-    graph = UnweightedGraphEV(args.rand_seed, args.n, args.m)
+    graph = UnweightedGraphEV(args.rand_seed, n, m, initialize_graph)
 elif "e" in args.model:
-    graph = UnweightedGraphE(args.rand_seed, args.n, args.m)
+    graph = UnweightedGraphE(args.rand_seed, n, m, initialize_graph)
 elif "v" in args.model:
-    graph = UnweightedGraphV(args.rand_seed, args.n, args.m)
+    graph = UnweightedGraphV(args.rand_seed, n, m, initialize_graph)
 else:
-    graph = UnweightedGraph(args.rand_seed, args.n, args.m)
+    graph = UnweightedGraph(args.rand_seed, n, m, initialize_graph)
 
-runner = Runner(args.probe_rate, args.change_rate, algorithm, graph)
-runner.run(args.iterations, args.visualization_step)
+runner = Runner(args.probe_rate, args.change_rate, algorithm, graph, use_dataset, dataset)
+runner.run(iterations, args.visualization_step)
 
-print()
-print("at the end of execution, m = {}, n = {}".format(graph.m, graph.n))
+if not use_dataset:
+    print()
+    print("at the end of execution, m = {}, n = {}".format(graph.m, graph.n))
 
 print()
 print("Correct answers: {}/{} ({}%)".format(runner.get_correct_answers(), runner.get_total_iterations(), round(100*runner.get_correct_answers()/runner.get_total_iterations(), 2)))
